ebff9b3d02c7d05b70db510743550e0b4102fec2
Dustin Scott <edge.fang@gmail.com>
2011-12-12 15:08:17 -0500

    test_suite code

---
+++ EngieBits/src/tests/test_suite.c
@@ -0,0 +1,14 @@
+/*
+ * test_suite.c
+ *
+ *  Created on: Dec 12, 2011
+ *      Author: dustin
+ */
+#include "test_suite.h"
+
+_Bool test_suite(){
+
+	return true;
+}
+
+
---
+++ EngieBits/src/tests/test_suite.h
@@ -0,0 +1,14 @@
+/*
+ * test_suite.h
+ *
+ *  Created on: Dec 12, 2011
+ *      Author: dustin
+ */
+#ifndef TEST_SUITE_H_
+#define TEST_SUITE_H_
+#include <stdbool.h>
+
+_Bool test_suite();
+
+
+#endif /* TEST_SUITE_H_ */
